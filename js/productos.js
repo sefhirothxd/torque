044c3446757e8165a6ed81d3f5189b4d723581fc
bryan vera <bveracachay@gmail.com>
2021-07-30 16:53:29 -0500

    se agregar el boton busqueda y se amplia producto

---
+++ js/productos.js
@@ -0,0 +1,83 @@
+let y =
+	window.scrollY +
+	document.querySelector('#min_navbar').getBoundingClientRect().top;
+let x =
+	window.innerWidth ||
+	document.documentElement.clientWidth ||
+	document.body.clientWidth;
+
+// if (x >= 1200) {
+// 	if (y > 50) {
+// 		document.querySelector('.product-container').style.top = '184px';
+// 		document.querySelector('.product-container').style.animationName =
+// 			'probando';
+// 	} else {
+// 		document.querySelector('.product-container').style.top = '150px';
+// 		document.querySelector('.product-container').style.animationName =
+// 			'example';
+// 	}
+// } else if (x < 1200) {
+// 	document.querySelector('.product-container').style.top = '150px';
+// } else if (x == 137) {
+// 	document.querySelector('.product-container').style.top = '150px';
+// }
+
+console.log(y);
+console.log(x, 'ancho');
+
+window.addEventListener('scroll', () => {
+	let y =
+		window.scrollY +
+		document.querySelector('#min_navbar').getBoundingClientRect().top;
+	let x =
+		window.innerWidth ||
+		document.documentElement.clientWidth ||
+		document.body.clientWidth;
+
+	console.log(y);
+	console.log(x, 'ancho');
+
+	if (x >= 1200) {
+		if (y > 50) {
+			document.querySelector('.product-container').style.top = '100px';
+			document.querySelector('.product-container').style.animationName =
+				'probando';
+		} else {
+			document.querySelector('.product-container').style.top = '150px';
+			document.querySelector('.product-container').style.animationName =
+				'example';
+		}
+	}
+	if (x < 1200 && x >= 992) {
+		if (y == 86) {
+			document.querySelector('.product-container').style.top = '186px';
+			document.querySelector('.product-container').style.animationName =
+				'medida1200';
+		} else {
+			document.querySelector('.product-container').style.top = '100px';
+			document.querySelector('.product-container').style.animationName =
+				'probando';
+		}
+	}
+	if (x < 992) {
+		if (y == 137) {
+			document.querySelector('.product-container').style.top = '236px';
+			document.querySelector('.product-container').style.animationName =
+				'medida92';
+		} else {
+			document.querySelector('.product-container').style.top = '100px';
+			document.querySelector('.product-container').style.animationName =
+				'probando';
+		}
+	}
+
+	// if (y > 50) {
+	// 	document.querySelector('.product-container').style.top = '100px';
+	// 	document.querySelector('.product-container').style.animationName =
+	// 		'probando';
+	// } else {
+	// 	document.querySelector('.product-container').style.top = '150px';
+	// 	document.querySelector('.product-container').style.animationName =
+	// 		'example';
+	// }
+});
