7f0477b0a94cfec39e1d18d6ea838734323aa7a7
bryan vera <bveracachay@gmail.com>
2021-07-31 19:52:34 -0500

    se soliciona problema del header que no tiene fix

--- js/productoHeader.js
+++ js/productoHeader.js
@@ -1,13 +1,14 @@
-let y =
-	window.scrollY +
-	document.querySelector('#main_navbar').getBoundingClientRect().top;
-let x =
-	window.innerWidth ||
-	document.documentElement.clientWidth ||
-	document.body.clientWidth;
+// let containerHeader = document.getElementById('main_navbar');
+// let y = window.pageYOffset || document.documentElement.scrollTop;
+// let x =
+// 	window.innerWidth ||
+// 	document.documentElement.clientWidth ||
+// 	document.body.clientWidth;
 
 let product = document.querySelector('.list-products');
 let productMovil = document.querySelector('.ocultar');
+
+let productContainer = document.querySelector('.product-container');
 
 // if (x >= 1200) {
 // 	if (y > 50) {
@@ -27,9 +28,7 @@ let productMovil = document.querySelector('.ocultar');
 
 setInterval(() => {
 	// console.log('verifico');
-	let y =
-		window.scrollY +
-		document.querySelector('#min_navbar').getBoundingClientRect().top;
+	let y = window.pageYOffset || document.documentElement.scrollTop;
 	let x =
 		window.innerWidth ||
 		document.documentElement.clientWidth ||
@@ -43,99 +42,27 @@ setInterval(() => {
 		productMovil.style.display = 'none';
 	}
 
-	if (x >= 1200) {
-		if (y > 50) {
-			document.querySelector('.product-container').style.top = '100px';
-			document.querySelector('.product-container').style.animationName =
-				'probando';
-		} else {
-			document.querySelector('.product-container').style.top = '150px';
-			document.querySelector('.product-container').style.animationName =
-				'example';
-		}
-	}
-	if (x < 1200 && x >= 992) {
-		if (y == 86) {
-			document.querySelector('.product-container').style.top = '186px';
-			document.querySelector('.product-container').style.animationName =
-				'medida1200';
-		} else {
-			document.querySelector('.product-container').style.top = '100px';
-			document.querySelector('.product-container').style.animationName =
-				'probando';
-		}
-	}
-	if (x < 992) {
-		if (y != 136) {
-			document.querySelector('.product-container').style.top = '236px';
-			document.querySelector('.product-container').style.animationName =
-				'medida92';
-		} else {
-			document.querySelector('.product-container').style.top = '100px';
-			document.querySelector('.product-container').style.animationName =
-				'probando';
-		}
-	}
+	// if (x >= 1200) {
+	// 	document.querySelector('.product-container').style.top = '150px';
+	// 	document.querySelector('.product-container').style.animationName =
+	// 		'example';
+	// }
+	// if (x < 1200 && x >= 992) {
+	// 	document.querySelector('.product-container').style.top = '150px';
+	// 	document.querySelector('.product-container').style.animationName =
+	// 		'medida1200';
+	// }
+	// if (x < 992) {
+	// 	document.querySelector('.product-container').style.top = '150px';
+	// 	document.querySelector('.product-container').style.animationName =
+	// 		'medida92';
+	// }
 }, 500);
 
-addEventListener('scroll', () => {
-	console.log(y, 'alto');
-	console.log(x, 'ancho');
-});
-
-// window.addEventListener('scroll', () => {
-// 	let y =
-// 		window.scrollY +
-// 		document.querySelector('#min_navbar').getBoundingClientRect().top;
-// 	let x =
-// 		window.innerWidth ||
-// 		document.documentElement.clientWidth ||
-// 		document.body.clientWidth;
-
-// 	console.log(y);
+// addEventListener('scroll', () => {
+// 	let containerHeader = document.getElementById('main_navbar');
+// 	let y = window.pageYOffset || document.documentElement.scrollTop;
+// 	let x = containerHeader.scrollLeft;
+// 	console.log(y, 'alto');
 // 	console.log(x, 'ancho');
-
-// 	if (x >= 1200) {
-// 		if (y > 50) {
-// 			document.querySelector('.product-container').style.top = '100px';
-// 			document.querySelector('.product-container').style.animationName =
-// 				'probando';
-// 		} else {
-// 			document.querySelector('.product-container').style.top = '150px';
-// 			document.querySelector('.product-container').style.animationName =
-// 				'example';
-// 		}
-// 	}
-// 	if (x < 1200 && x >= 992) {
-// 		if (y == 86) {
-// 			document.querySelector('.product-container').style.top = '186px';
-// 			document.querySelector('.product-container').style.animationName =
-// 				'medida1200';
-// 		} else {
-// 			document.querySelector('.product-container').style.top = '100px';
-// 			document.querySelector('.product-container').style.animationName =
-// 				'probando';
-// 		}
-// 	}
-// 	if (x < 992) {
-// 		if (y == 137) {
-// 			document.querySelector('.product-container').style.top = '236px';
-// 			document.querySelector('.product-container').style.animationName =
-// 				'medida92';
-// 		} else {
-// 			document.querySelector('.product-container').style.top = '100px';
-// 			document.querySelector('.product-container').style.animationName =
-// 				'probando';
-// 		}
-// 	}
-
-// if (y > 50) {
-// 	document.querySelector('.product-container').style.top = '100px';
-// 	document.querySelector('.product-container').style.animationName =
-// 		'probando';
-// } else {
-// 	document.querySelector('.product-container').style.top = '150px';
-// 	document.querySelector('.product-container').style.animationName =
-// 		'example';
-// }
 // });
