8b7ebc8c0309c3795a25553fd1017f01fe094bc5
bryan vera <bveracachay@gmail.com>
2021-08-27 17:58:48 -0500

    se cambia el sliader

--- js/glinder.js
+++ js/glinder.js
@@ -3,6 +3,9 @@ window.addEventListener('load', function () {
 		slidesToShow: 1,
 		dots: '#dots',
 		draggable: true,
+		slidesToScroll: 1,
+		itemWidth: 150,
+		duration: 0.25,
 		arrows: {
 			prev: '.glider-prev',
 			next: '.glider-next',
